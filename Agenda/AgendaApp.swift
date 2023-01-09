4fc907a88fe0c6c9d492994afc39c47706899373
martins107 <martincabezosvalle@gmail.com>
2023-01-09 20:15:45 +0100

    Initial Commit

---
+++ Agenda/AgendaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgendaApp.swift
+//  Agenda
+//
+//  Created by Apps2T on 9/1/23.
+//
+
+import SwiftUI
+
+@main
+struct AgendaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
